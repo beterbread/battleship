5706d6a7781ed7059f44c2ab0b5f531fdfeb7169
Peter Kim <petertimmmybob113@gmail.com>
2023-12-26 05:40:10 -0600

    Improved styling

--- src/index.js
+++ src/index.js
@@ -1,259 +1,251 @@
 function Ship(user_length = 0) {
-    let length = user_length;
-    let hits = 0;
-    const hit = () => {
-        hits++;
-    }
-    const isSunk = () => {
-        return length === hits;
-    };
-    return { hit, isSunk }
+  const length = user_length;
+  let hits = 0;
+  const hit = () => {
+    hits++;
+  };
+  const isSunk = () => length === hits;
+  return { hit, isSunk };
 }
 
 function Gameboard() {
-    let board = Array.from({ length: 10 }, () => Array(10).fill(0));
-    let ships = [];
+  let board = Array.from({ length: 10 }, () => Array(10).fill(0));
+  let ships = [];
 
-    const placeShip = (x, y, length, isVertical) => {
-        if (x < 0 || y < 0 || x >= 10 || y >= 10) {
-            return false;
-        }
-        const checkBounds = isVertical ? y + length : x + length;
-        if (!isVertical && checkBounds > 10) {
-            return false;
-        } else if (isVertical && checkBounds > 10) {
-            return false;
-        }
-        for (let i = 0; i < length; i++) {
-            const currentX = isVertical ? x : i + x;
-            const currentY = isVertical ? i + y : y;
-            if (board[currentY][currentX] !== 0) {
-                return false;
-            }
-        }
-        const ship = Ship(length);
-        ships.push(ship);
-        for (let i = 0; i < length; i++) {
-            const currentX = isVertical ? x : i + x;
-            const currentY = isVertical ? i + y : y;
-            board[currentY][currentX] = ship;
-        }
-    
-        return true;
-    };
-    
+  const placeShip = (x, y, length, isVertical) => {
+    if (x < 0 || y < 0 || x >= 10 || y >= 10) {
+      return false;
+    }
+    const checkBounds = isVertical ? y + length : x + length;
+    if (!isVertical && checkBounds > 10) {
+      return false;
+    } if (isVertical && checkBounds > 10) {
+      return false;
+    }
+    for (let i = 0; i < length; i++) {
+      const currentX = isVertical ? x : i + x;
+      const currentY = isVertical ? i + y : y;
+      if (board[currentY][currentX] !== 0) {
+        return false;
+      }
+    }
+    const ship = Ship(length);
+    ships.push(ship);
+    for (let i = 0; i < length; i++) {
+      const currentX = isVertical ? x : i + x;
+      const currentY = isVertical ? i + y : y;
+      board[currentY][currentX] = ship;
+    }
 
-    const receiveAttack = (x, y) => {
-        if (x < 0 || y < 0 || x >= 10 || y >= 10) {
-            return false;
-        }
-        if (board[x][y] === 'missed' || board[x][y] === 'hit') {
-            return false;
-        }
-        const ship = board[x][y];
-        if (ship) {
-            ship.hit();
-            board[x][y] = 'hit';
+    return true;
+  };
+
+  const receiveAttack = (x, y) => {
+    if (x < 0 || y < 0 || x >= 10 || y >= 10) {
+      return false;
+    }
+    if (board[x][y] === 'missed' || board[x][y] === 'hit') {
+      return false;
+    }
+    const ship = board[x][y];
+    if (ship) {
+      ship.hit();
+      board[x][y] = 'hit';
+    } else {
+      board[x][y] = 'missed';
+      return false;
+    }
+  };
+
+  const allSunk = () => ships.every((ship) => ship.isSunk());
+
+  const getGrid = () => {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        const currentCell = board[i][j];
+        if (currentCell === 'missed') {
+          tile.classList.add('missed');
+        } else if (currentCell === 'hit') {
+          tile.classList.add('hit');
+        } else if (currentCell === 0) {
+          tile.classList.add('tile');
         } else {
-            board[x][y] = 'missed';
-            return false;
+          tile.classList.add('ship');
         }
-    };
-
-    const allSunk = () => {
-        return ships.every(ship => ship.isSunk());
+        tile.setAttribute('id', `${i} ${j}`);
+        grid.appendChild(tile);
+      }
     }
+    return grid;
+  };
 
-    const getGrid = () => {
-        let grid = document.createElement('div');
-        grid.classList.add('grid');
-        for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 10; j++) {
-                let tile = document.createElement('div');
-                tile.classList.add('tile');
-                const currentCell = board[i][j];
-                if (currentCell === 'missed') {
-                    tile.classList.add('missed');
-                } else if (currentCell === 'hit') {
-                    tile.classList.add('hit');
-                } else if (currentCell === 0) {
-                    tile.classList.add('tile');
-                } else {
-                    tile.classList.add('ship');
-                }
-                tile.setAttribute("id", i + " " + j);
-                grid.appendChild(tile);
-            }
-        }
-        return grid;
-    };
-    
-    const resetBoard = () => {
-        board = Array.from({ length: 10 }, () => Array(10).fill(0));
-        ships = [];
+  const resetBoard = () => {
+    board = Array.from({ length: 10 }, () => Array(10).fill(0));
+    ships = [];
+  };
+
+  const checkHit = (x, y) => {
+    if (board[x][y] === 'hit' || board[x][y] === 'missed') {
+      return false;
     }
+    return true;
+  };
 
-    const checkHit = (x, y) => {
-        if (board[x][y] === 'hit' || board[x][y] === 'missed') {
-            return false;
-        }
-        return true;
-    }
-
-    return { placeShip, receiveAttack, allSunk, getGrid, resetBoard, checkHit };
+  return {
+    placeShip, receiveAttack, allSunk, getGrid, resetBoard, checkHit,
+  };
 }
 
 const Player = () => {
-    const attack = (gameboard, x, y) => {
-        gameboard.receiveAttack(x, y);
-    };
+  const attack = (gameboard, x, y) => {
+    gameboard.receiveAttack(x, y);
+  };
 
-    return { attack };
+  return { attack };
 };
 
 const ComputerPlayer = () => {
-    const attack = (gameboard) => {
-        let x = Math.floor(Math.random() * 10);
-        let y = Math.floor(Math.random() * 10);
-        while (gameboard.checkHit(x, y) === false) {
-            x = Math.floor(Math.random() * 10);
-            y = Math.floor(Math.random() * 10);
-        }
-        gameboard.receiveAttack(x, y);
-    };
+  const attack = (gameboard) => {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+    while (gameboard.checkHit(x, y) === false) {
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * 10);
+    }
+    gameboard.receiveAttack(x, y);
+  };
 
-    return { attack };
+  return { attack };
 };
 
 // Logic for placing ships on computer's board
-function initializeCompGB (cboard, compGB) {
-    let x = Math.floor(Math.random() * 10) + 1;
-    let y = Math.floor(Math.random() * 10) + 1;
-    let coin = Math.floor(Math.random() * 2) + 1;
-    let vertical = (coin === 1);
-    let lengths = [2, 3, 3, 4, 5];
-    for (let i = 0; i < 5; i++) {
-        while (compGB.placeShip(parseInt(x) - 1, parseInt(y) - 1, lengths[i], vertical) === false) {
-            x = Math.floor(Math.random() * 10) + 1;
-            y = Math.floor(Math.random() * 10) + 1;
-            coin = Math.floor(Math.random() * 2) + 1;
-            vertical = (coin === 1);
-        }  
+function initializeCompGB(cboard, compGB) {
+  let x = Math.floor(Math.random() * 10) + 1;
+  let y = Math.floor(Math.random() * 10) + 1;
+  let coin = Math.floor(Math.random() * 2) + 1;
+  let vertical = (coin === 1);
+  const lengths = [2, 3, 3, 4, 5];
+  for (let i = 0; i < 5; i++) {
+    while (compGB.placeShip(parseInt(x) - 1, parseInt(y) - 1, lengths[i], vertical) === false) {
+      x = Math.floor(Math.random() * 10) + 1;
+      y = Math.floor(Math.random() * 10) + 1;
+      coin = Math.floor(Math.random() * 2) + 1;
+      vertical = (coin === 1);
     }
-    cboard.removeChild(cboard.firstChild);
-    cboard.append(compGB.getGrid());
+  }
+  cboard.removeChild(cboard.firstChild);
+  cboard.append(compGB.getGrid());
 }
 
 function showModalOverlay() {
-    document.getElementById('modalOverlay').classList.add('active');
+  document.getElementById('modalOverlay').classList.add('active');
 }
 
 function hideModalOverlay() {
-    document.getElementById('modalOverlay').classList.remove('active');
+  document.getElementById('modalOverlay').classList.remove('active');
 }
 
 // Objects for main loop
-let playerGB = Gameboard();
-let compGB = Gameboard();
-let player = Player();
-let compPlayer = ComputerPlayer();
+const playerGB = Gameboard();
+const compGB = Gameboard();
+const player = Player();
+const compPlayer = ComputerPlayer();
 
 // Boards displayed
-let pboard = document.querySelector('#pboard');
+const pboard = document.querySelector('#pboard');
 pboard.appendChild(playerGB.getGrid());
-let cboard = document.querySelector('#cboard');
+const cboard = document.querySelector('#cboard');
 cboard.appendChild(compGB.getGrid());
 
-initializeCompGB (cboard, compGB);
+initializeCompGB(cboard, compGB);
 showModalOverlay();
 
 // Logic for the popup form
-let popup = document.querySelector('#popup');
-let submit = document.querySelector('#formSubmit');
-let error = document.querySelector('#error');
-submit.addEventListener('click', function (event) {
-    let lengths = [2, 3, 3, 4, 5];
-    let coordinateElements = document.querySelectorAll('.coordinates');
-    let err = true;
-    coordinateElements.forEach(function (coordinateElement, index) {
-        let x = coordinateElement.querySelector('input:nth-child(1)').value;
-        let y = coordinateElement.querySelector('input:nth-child(2)').value;
-        let vertical = coordinateElement.querySelector('input[type="checkbox"]').checked;
-        let length = lengths[index];
-        if (playerGB.placeShip(parseInt(x) - 1, parseInt(y) - 1, length, vertical) === false) {
-            err = false;
-            return;
-        }
-    });
-    if (err === true) {
-        hideModalOverlay();
-        pboard.removeChild(pboard.firstChild);
-        pboard.append(playerGB.getGrid());
-        popup.style.display = "none";
-        // Main game 
-        // Define the event handler function
-    const clickHandler = function(event) {
-    const clickedTile = event.target;
-    if (clickedTile.classList.contains('tile')) {
-        let str1 = clickedTile.getAttribute("class");
-        if (!str1.includes("missed") && !str1.includes("hit")) {
-            let str2 = clickedTile.getAttribute("id");
-            let coordinates = str2.split(" ");
-            player.attack(compGB, parseInt(coordinates[0]), parseInt(coordinates[1]))  
-            cboard.removeChild(cboard.firstChild);
-            cboard.append(compGB.getGrid());
-            compPlayer.attack(playerGB);
-            pboard.removeChild(pboard.firstChild);
-            pboard.append(playerGB.getGrid());
-            // Check for winner
-            let winPop = document.querySelector('.winPop');
-            let winner = document.querySelector('.winner');
-            if (compGB.allSunk() && playerGB.allSunk()) {
-                winPop.style.display = 'block';
-                winner.innerHTML = 'Tied game!';
-                showModalOverlay();
-            }
-            else if (compGB.allSunk()) {
-                winPop.style.display = 'block';
-                winner.innerHTML = 'You win!';
-                showModalOverlay();
-            }
-            else if (playerGB.allSunk()) {
-                winPop.style.display = 'block';
-                winner.innerHTML = 'You lose!';
-                showModalOverlay();
-            }
-            // Reset game
-            let again = document.querySelector('.again');
-            again.addEventListener('click', () => {
-                // Remove the event listener before resetting
-                cboard.removeEventListener('click', clickHandler);
-                hideModalOverlay();
-                
-                playerGB.resetBoard();
-                compGB.resetBoard();
-                pboard.removeChild(pboard.firstChild);
-                cboard.removeChild(cboard.firstChild);
-                pboard.appendChild(playerGB.getGrid());
-                cboard.appendChild(compGB.getGrid());
-                initializeCompGB (cboard, compGB);
-                popup.style.display = 'block';
-                winPop.style.display = 'none';
-            });
-        }
+const popup = document.querySelector('#popup');
+const submit = document.querySelector('#formSubmit');
+const error = document.querySelector('#error');
+submit.addEventListener('click', (event) => {
+  const lengths = [2, 3, 3, 4, 5];
+  const coordinateElements = document.querySelectorAll('.coordinates');
+  let err = true;
+  coordinateElements.forEach((coordinateElement, index) => {
+    const x = coordinateElement.querySelector('input:nth-child(1)').value;
+    const y = coordinateElement.querySelector('input:nth-child(2)').value;
+    const vertical = coordinateElement.querySelector('input[type="checkbox"]').checked;
+    const length = lengths[index];
+    if (playerGB.placeShip(parseInt(x) - 1, parseInt(y) - 1, length, vertical) === false) {
+      err = false;
     }
+  });
+  if (err === true) {
+    hideModalOverlay();
+    pboard.removeChild(pboard.firstChild);
+    pboard.append(playerGB.getGrid());
+    popup.style.display = 'none';
+    // Main game
+    // Define the event handler function
+    const clickHandler = function (event) {
+      const clickedTile = event.target;
+      if (clickedTile.classList.contains('tile')) {
+        const str1 = clickedTile.getAttribute('class');
+        if (!str1.includes('missed') && !str1.includes('hit')) {
+          const str2 = clickedTile.getAttribute('id');
+          const coordinates = str2.split(' ');
+          player.attack(compGB, parseInt(coordinates[0]), parseInt(coordinates[1]));
+          cboard.removeChild(cboard.firstChild);
+          cboard.append(compGB.getGrid());
+          compPlayer.attack(playerGB);
+          pboard.removeChild(pboard.firstChild);
+          pboard.append(playerGB.getGrid());
+          // Check for winner
+          const winPop = document.querySelector('.winPop');
+          const winner = document.querySelector('.winner');
+          if (compGB.allSunk() && playerGB.allSunk()) {
+            winPop.style.display = 'block';
+            winner.innerHTML = 'Tied game!';
+            showModalOverlay();
+          } else if (compGB.allSunk()) {
+            winPop.style.display = 'block';
+            winner.innerHTML = 'You win!';
+            showModalOverlay();
+          } else if (playerGB.allSunk()) {
+            winPop.style.display = 'block';
+            winner.innerHTML = 'You lose!';
+            showModalOverlay();
+          }
+          // Reset game
+          const again = document.querySelector('.again');
+          again.addEventListener('click', () => {
+            // Remove the event listener before resetting
+            cboard.removeEventListener('click', clickHandler);
+
+            playerGB.resetBoard();
+            compGB.resetBoard();
+            pboard.removeChild(pboard.firstChild);
+            cboard.removeChild(cboard.firstChild);
+            pboard.appendChild(playerGB.getGrid());
+            cboard.appendChild(compGB.getGrid());
+            initializeCompGB(cboard, compGB);
+            popup.style.display = 'block';
+            winPop.style.display = 'none';
+          });
+        }
+      }
     };
     cboard.addEventListener('click', clickHandler);
-    }
-    else {
-        playerGB.resetBoard();
-        error.style.display = "block";
-    }
+  } else {
+    playerGB.resetBoard();
+    error.style.display = 'block';
+  }
 });
 
 module.exports = {
-    Ship,
-    Gameboard,
-    Player,
-    ComputerPlayer
+  Ship,
+  Gameboard,
+  Player,
+  ComputerPlayer,
 };
--- src/ship.test.js
+++ src/ship.test.js
@@ -2,16 +2,16 @@ const { Ship, Gameboard } = require('./index.js');
 
 // Ship tests
 describe('Ship', () => {
-    test('Not sunk yet', () => {
-        const ship = Ship(5);
-        expect(ship.isSunk()).toBe(false);
-    });
+  test('Not sunk yet', () => {
+    const ship = Ship(5);
+    expect(ship.isSunk()).toBe(false);
+  });
 
-    test('Sunk', () => {
-        const ship = Ship(3);
-        ship.hit();
-        ship.hit();
-        ship.hit();
-        expect(ship.isSunk()).toBe(true);
-    });
+  test('Sunk', () => {
+    const ship = Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });
